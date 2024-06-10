29278e710535c289b0ac00765065271017b6467a
Mikias Goitom <mikiasgoitom7@gmail.com>
2024-06-10 23:09:13 +0300

    The is the first half of the page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,17 @@ import Partners from "./Partners";
 import Testimonials from "./Testimonials";
 import Footer from "./Footer";
 import Services from "./Services";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Vision_Mission from "./Vision_Mission";
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
+        <Navbar />
+        <Hero />
+        <Vision_Mission />
         <Services />
         <Testimonials />
         <Partners />
