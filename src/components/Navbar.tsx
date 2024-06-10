c81939e27eecc7c7a72c3d2968fc0084b92e6981
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-06-10 14:55:01 -0700

    run formatter, fix wite page on gh-pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,23 @@ const Navbar = () => {
     <div className="navbar h-20 bg-gray-50 fixed px-20 z-10">
       <div className="navbar-start">
         <img src="/icon.png" alt="logo" className="w-8 h-9 mx-5" />
-        <a className="btn btn-ghost text-green-950 text-xl hover:text-green-950">Har Consultancy</a>
+        <a className="btn btn-ghost text-green-950 text-xl hover:text-green-950">
+          Har Consultancy
+        </a>
       </div>
       <div className="navbar-end pr-20">
-        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">Home</a>
-        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">About us</a>
-        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">Services</a>
-        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">Contact Us</a>
+        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">
+          Home
+        </a>
+        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">
+          About us
+        </a>
+        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">
+          Services
+        </a>
+        <a className="btn btn-ghost text-green-950 hover:text-green-950 text-base">
+          Contact Us
+        </a>
       </div>
     </div>
   );
--- src/components/Vision_Mission.tsx
+++ src/components/Vision_Mission.tsx
@@ -2,13 +2,23 @@ const Vision_Mission = () => {
   return (
     <div className="flex w-full min-h-screen justify-center bg-gray-50 z-0">
       <div className="grid h-fit flex-grow card bg-gray-50 text-black rounded-box place-items-center mx-5 self-center">
-        <h2 className="text-2xl text-green-950 mb-10 tracking-widest">VISION</h2>
-        <p className="tracking-wider text-justify leading-6 text-base bg-gray-50">Building a network of strong and sustainable community organizations in Ethiopia, driving positive change and lasting impact.</p>
+        <h2 className="text-2xl text-green-950 mb-10 tracking-widest">
+          VISION
+        </h2>
+        <p className="tracking-wider text-justify leading-6 text-base bg-gray-50">
+          Building a network of strong and sustainable community organizations
+          in Ethiopia, driving positive change and lasting impact.
+        </p>
       </div>
 
       <div className="grid h-fit flex-grow card bg-gray-50 text-black rounded-box place-items-center mx-5 self-center">
-        <h2 className="text-2xl text-green-950 mb-10 tracking-widest">MISSION</h2>
-        <p className="tracking-wider text-justify leading-6 text-base bg-gray-50">Empowering impact organizations in Ethiopia to maximize their potential through strategic consulting services.</p>
+        <h2 className="text-2xl text-green-950 mb-10 tracking-widest">
+          MISSION
+        </h2>
+        <p className="tracking-wider text-justify leading-6 text-base bg-gray-50">
+          Empowering impact organizations in Ethiopia to maximize their
+          potential through strategic consulting services.
+        </p>
       </div>
     </div>
   );
