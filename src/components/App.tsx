09b76b7283e991cae0ea72364fd0c8407d441a26
Mikias Goitom <mikiasgoitom7@gmail.com>
2024-07-28 13:46:16 +0300

    created a file that manage routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,17 @@
 import "../styles/App.css";
-import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import Home from "../pages/Home";
 
 const App: React.FC = () => {
-  const basePath = import.meta.env.VITE_BASE_PATH || "";
   return (
-    <Router basename={basePath}>
-      <div className="flex flex-col min-h-screen">
-        <div className="flex-grow">
-          <Navbar />
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <Home />
-                  </>
-                }
-              />
-              <Route path="/Jobs"></Route>
-              {/* additional routes here */}
-            </Routes>
-          </Suspense>
-        </div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Navbar />
+        <Outlet />
         <Footer />
       </div>
-    </Router>
+    </div>
   );
 };
 
---
+++ src/router/Router.tsx
@@ -0,0 +1,29 @@
+import { Suspense, lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+const App = lazy(() => import("../components/App"));
+const Home = lazy(() => import("../pages/Home"));
+
+const basePath = import.meta.env.VITE_BASE_PATH || "";
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+export default Router;
+
