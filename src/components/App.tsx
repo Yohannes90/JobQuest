87078cff2124cbce1f50791abb22c7cf484a09a4
Mikias Goitom <mikiasgoitom7@gmail.com>
2024-07-29 00:24:31 +0300

    made job listing uptodate

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,15 +3,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-/**
- * Main application component.
- * Renders the entire application structure including navigation, lazy-loaded routes,
- * and a footer.
- *
- * @returns JSX element representing the entire application layout.
- */
 const App: React.FC = () => {
-
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
