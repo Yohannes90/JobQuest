749a68d35ca1573669f74b8fd112fbd5af7d12b2
Mikias Goitom <mikiasgoitom7@gmail.com>
2024-09-09 19:26:44 +0300

    added the blog files

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,24 @@
+import { useLocation } from "react-router-dom";
+
+interface Blog {
+  id: number;
+  blogTitle: string;
+  blogCategory:
+    | "technology"
+    | "business"
+    | "lifestyle"
+    | "education"
+    | "health"
+    | "entertainment";
+  content: string;
+  image: string;
+  publishedAt: string;
+}
+
+const Blog: React.FC = () => {
+  const location = useLocation();
+  const { blog } = location.state as { blog: Blog };
+  return <div>Blog</div>;
+};
+
+export default Blog;
